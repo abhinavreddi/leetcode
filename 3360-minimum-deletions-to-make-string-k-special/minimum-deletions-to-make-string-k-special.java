4ff6c42aa152a0ae70bd8e1d17b91d08e7359d6b
Abhinav Nalasani <144915418+abhinavreddi@users.noreply.github.com>
2025-06-21 23:21:58 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (8.51%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -1,34 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int minimumDeletions(String word, int k) {
-        HashMap<Character, Integer> freq = new HashMap<>();
-        
-        for (char c : word.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            freq[ch - 'a']++;
         }
-        
-
-        List<Integer> freqList = new ArrayList<>(freq.values());
-        Collections.sort(freqList);
-        
-        int minDel = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < freqList.size(); i++) {
-            int deletions = 0;
-            int target = freqList.get(i);
-            
-            for (int f : freqList) {
-                if (f > target + k) {
-                    deletions += f - (target + k);
-                } else if (f < target) {
-                    deletions += f;
-                }
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i] == 0) continue;
+            int freqEle = freq[i];
+            int max = freqEle + k;
+            int ans = 0;
+            for(int j=0;j<freq.length;j++){
+                if(freq[j] < freqEle) ans = ans + freq[j];
+                else if(freq[j] > max) ans = ans + freq[j] - max;
             }
-            
-            minDel = Math.min(minDel, deletions);
+            min = Math.min(min, ans);
         }
-        
-        return minDel;
+        return min;
     }
 }
