29911a4cf3ce5efcbcd8dab83f62d76a2aa85d6a
Abhinav Nalasani <144915418+abhinavreddi@users.noreply.github.com>
2025-07-15 23:09:40 +0530

    Time: 7 ms (9.93%) | Memory: 43.8 MB (5.96%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isValid(String word) {
-        if(word.length()>=3 && word.matches("[a-zA-Z0-9]+") && word.matches(".*[aeiouAEIOU].*") && word.matches(".*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ].*")){
+        if(word.length()<3 ){
+            return false;
+        }else if(word.matches("[a-zA-Z0-9]+") && word.matches(".*[aeiouAEIOU].*") && word.matches(".*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ].*")){
             return true;
         }
         return false;
