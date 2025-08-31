e2a12b520fd6f3fd4a0ad21a9b5c69bccf04a056
Abhinav Nalasani <144915418+abhinavreddi@users.noreply.github.com>
2025-08-31 14:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (61.36%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,40 +1,37 @@
 class Solution {
     public int maximum69Number (int num) {
-        // int temp = num;
-        // int res = 1;
-        // boolean changed = false;
-        // int pos = (int)Math.log10(num)+1;
+      int temp = num;
+        int res = 0;
+        boolean changed = false;
+        int pos = (int)Math.log10(num);
 
+        while (pos >= 0) {
+            int digit = (num / (int)Math.pow(10, pos)) % 10;
+            if (digit == 6 && !changed) {
+                res = res * 10 + 9;
+                changed = true;
+            } else {
+                res = res * 10 + digit;
+            }
+            pos--;
+        }
+        return res;
+
+
+        // int temp = num;
+        // int index = -1;
+        // int pos = 0;
         // while(temp>0){
-        //     int digit = num % (int)Math.pow(10,pos);
-        //     if(digit== 9){
-        //         res=digit+res*(int)Math.pow(10,pos);
-        //         pos=pos-1;
-        //     }else{
-        //         res = 9+res*(int)Math.pow(10,pos);
-        //         pos=pos-1;
+        //     int digit = temp %10;
+        //     if(digit == 6){
+        //         index = pos;
         //     }
-        //     temp/=10;
-            
+        //     temp = temp/10;
+        //     pos++;
         // }
 
-        // return res;
+        // if(index == -1) return num;
 
-
-        int temp = num;
-        int index = -1;
-        int pos = 0;
-        while(temp>0){
-            int digit = temp %10;
-            if(digit == 6){
-                index = pos;
-            }
-            temp = temp/10;
-            pos++;
-        }
-
-        if(index == -1) return num;
-
-        return num+(int) Math.pow(10,index)*3;
+        // return num+(int) Math.pow(10,index)*3;
     }
 }
